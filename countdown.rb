778756d505cd66ab37a70ede5c9bf1e1e0144bbd
Lizzie Chan <lchan217@gmail.com>
2019-02-20 02:12:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -13,7 +13,7 @@ def countdown_with_sleep
     until seconds  == 0 
       puts "#{seconds} SECOND(S)!"
       seconds -= 1 
-      sleep1
+      sleep(1)
     end 
   end
   return "HAPPY NEW YEAR!"
