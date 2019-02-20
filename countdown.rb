99ebda6f437bc6c6b6c8bddf0f1d181b8fbd4e9b
Lizzie Chan <lchan217@gmail.com>
2019-02-20 02:11:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,9 @@
 def countdown(seconds)
-  until seconds  == 0 
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1 
-  end 
+  if seconds > 0 
+    until seconds  == 0 
+      puts "#{seconds} SECOND(S)!"
+      seconds -= 1 
+    end 
+  else 
+    "HAPPY NEW YEAR!"
 end 
